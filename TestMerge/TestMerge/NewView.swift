871ef09966782877784cb5f57f151f0a0a6febd3
Amandine Cousin <cousin.amandine.pro@gmail.com>
2024-06-05 14:07:22 +0200

    test

---
+++ TestMerge/TestMerge/NewView.swift
@@ -0,0 +1,18 @@
+//
+//  NewView.swift
+//  TestMerge
+//
+//  Created by Amandine Cousin on 05/06/2024.
+//
+
+import SwiftUI
+
+struct NewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewView()
+}
