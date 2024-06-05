b678d67c2b893fbc0a24f53d0cc6fe79beb8ea11
Amandine Cousin <cousin.amandine.pro@gmail.com>
2024-06-05 12:22:00 +0200

    Init project

---
+++ TestMerge/TestMerge/TestMergeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestMergeApp.swift
+//  TestMerge
+//
+//  Created by Amandine Cousin on 05/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestMergeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
